9ee2375796b3edfba44199c8956159e8a560c64a
jinghaihan <jhh19980114@gmail.com>
2026-05-06 22:29:37 +0800

    chore: update sponsorkit config

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,3 +1,27 @@
-import { defineConfig } from 'sponsorkit'
+import { defineConfig, tierPresets } from 'sponsorkit'
 
-export default defineConfig({})
+export default defineConfig({
+  mode: 'sponsors',
+  providers: ['github'],
+  tiers: [
+    {
+      title: 'Backer',
+      preset: tierPresets.small,
+    },
+    {
+      title: 'Sponsor',
+      monthlyDollars: 10,
+      preset: tierPresets.base,
+    },
+    {
+      title: 'Silver Supporter',
+      monthlyDollars: 50,
+      preset: tierPresets.medium,
+    },
+    {
+      title: 'Gold Supporter',
+      monthlyDollars: 100,
+      preset: tierPresets.large,
+    },
+  ],
+})
